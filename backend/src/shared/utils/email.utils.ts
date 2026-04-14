622aa74856707485807b40205d8b48141afcc4ae
Omar Mahmoud <omar.mahmoud200210@gmail.com>
2026-04-14 21:20:23 +0200

    fix email utils issue

--- backend/src/shared/utils/email.utils.ts
+++ backend/src/shared/utils/email.utils.ts
@@ -9,45 +9,24 @@ interface EmailOptions {
 }
 
 const sendEmail = async (options: EmailOptions) => {
-  if (process.env.NODE_ENV === "development") {
-    const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST || "sandbox.smtp.mailtrap.io",
-      port: Number(process.env.EMAIL_PORT) || 2525,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST || "sandbox.smtp.mailtrap.io",
+    port: Number(process.env.EMAIL_PORT) || 2525,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-    const info = await transporter.sendMail({
-      from: '"Sukoon Productivity App" <no-reply@momentum.com>',
-      to: options.to,
-      subject: options.subject,
-      text: options.text,
-      html: options.html,
-    });
+  const info = await transporter.sendMail({
+    from: '"Sukoon Productivity App" <no-reply@momentum.com>',
+    to: options.to,
+    subject: options.subject,
+    text: options.text,
+    html: options.html,
+  });
 
-    logger.info(`Message sent: ${info.messageId}`);
-  } else {
-    
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.GMAIL_EMAIL,
-        pass: process.env.GMAIL_PASSWORD,
-      },
-    });
-
-    const info = await transporter.sendMail({
-      from: `"Sukoon Productivity App" <${process.env.GMAIL_EMAIL}>`,
-      to: options.to,
-      subject: options.subject,
-      text: options.text,
-      html: options.html,
-    });
-
-    logger.error(`Message sent: ${info.messageId}`);
-  }
+  logger.info(`Message sent: ${info.messageId}`);
 };
 
 export default sendEmail;
