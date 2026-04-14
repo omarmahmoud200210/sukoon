a92a008e2d2a1c73b0a51a74dc311626bf84e8c6
Omar Mahmoud <omar.mahmoud200210@gmail.com>
2026-04-14 21:27:29 +0200

    fix email utils issue

--- backend/src/shared/utils/email.utils.ts
+++ backend/src/shared/utils/email.utils.ts
@@ -9,17 +9,31 @@ interface EmailOptions {
 }
 
 const sendEmail = async (options: EmailOptions) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST || "sandbox.smtp.mailtrap.io",
-    port: Number(process.env.EMAIL_PORT) || 2525,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  let transporter;
+
+  if (process.env.GMAIL_EMAIL && process.env.GMAIL_PASSWORD) {
+    transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_EMAIL,
+        pass: process.env.GMAIL_PASSWORD,
+      },
+    });
+  } else {
+    transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST || "sandbox.smtp.mailtrap.io",
+      port: Number(process.env.EMAIL_PORT) || 2525,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+  }
+
+  const senderEmail = process.env.GMAIL_EMAIL || "no-reply@momentum.com";
 
   const info = await transporter.sendMail({
-    from: '"Sukoon Productivity App" <no-reply@momentum.com>',
+    from: `"Sukoon Productivity App" <${senderEmail}>`,
     to: options.to,
     subject: options.subject,
     text: options.text,
