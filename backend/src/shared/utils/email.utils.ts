4c5d8fd8d5b4a074d8a430231a39d60838672220
Omar Mahmoud <omar.mahmoud200210@gmail.com>
2026-04-17 14:09:58 +0200

    fix the verification email issue

--- backend/src/shared/utils/email.utils.ts
+++ backend/src/shared/utils/email.utils.ts
@@ -1,39 +1,20 @@
-import nodemailer from "nodemailer";
 import logger from "./logger.js";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (userEmail: string, token: string) => {
-  let transporter;
-
   if (process.env.NODE_ENV === "production") {
-    transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.GMAIL_EMAIL,
-        pass: process.env.GMAIL_PASSWORD,
-      },
+    resend.emails.send({
+      from: process.env.GMAIL_EMAIL!,
+      to: userEmail,
+      subject: "Verify Email",
+      text: "Verify your email",
+      html: `<a href="${process.env.FRONTEND_URL}/verify-email?token=${token}">Verify Email</a>`,
     });
-  } else {
-    transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST || "sandbox.smtp.mailtrap.io",
-      port: Number(process.env.EMAIL_PORT) || 2525,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+
+    logger.info(`Email sent to ${userEmail}`);
   }
-
-  const mailOptions = {
-    from: "omar.mahmoud200210@gmail.com",
-    to: userEmail,
-    subject: "Test Email",
-    text: "This is a test email",
-    html: `<h1>This is a test email</h1><a href="${process.env.FRONTEND_URL}/verify-email?token=${token}">Verify Email</a>`,
-  };
-
-  const info = await transporter.sendMail(mailOptions);
-
-  logger.info(`Message sent: ${info.messageId}`);
 };
 
 export default sendEmail;
